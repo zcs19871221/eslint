b7ad693a9eb960e205dd1c679ff32df024319299
zcs <530627552@qq.com>
2019-02-20 15:51:07 +0800

    lf

--- createDoc/createContent.js
+++ createDoc/createContent.js
@@ -43,7 +43,7 @@ const createContent = (file, linkTemplate) => {
     '@right': {
       titleName: '正确例子',
       bodyStyle: 'code',
-    }
+    },
   };
   const rulesName = [];
   let content = '';
@@ -53,9 +53,11 @@ const createContent = (file, linkTemplate) => {
       rulesName.push(ruleName);
       const ruleTitle = `<a id='${ruleName}'></a>\n## ${ruleName}\n\n`;
       const ruleContent = createSubContent(doc, keyMapDocStyle);
-      const eslintLink = `[eslint](${linkTemplate(ruleName.replace(/^.*?\//, ''))})\n\n`;
-      const jumpContentTable = '**[⬆ 回到目录](#目录)**\n\n'
-      content += `${ruleTitle}${ruleContent}${eslintLink}${jumpContentTable}`
+      const eslintLink = `[eslint](${linkTemplate(
+        ruleName.replace(/^.*?\//, ''),
+      )})\n\n`;
+      const jumpContentTable = '**[⬆ 回到目录](#目录)**\n\n';
+      content += `${ruleTitle}${ruleContent}${eslintLink}${jumpContentTable}`;
     },
   );
   return {
