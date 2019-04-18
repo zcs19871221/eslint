74f24f383c2615a306ebe614ddf068652ee32b48
zcs <530627552@qq.com>
2019-04-18 16:44:09 +0800

    mm

--- custom/ask.js
+++ custom/ask.js
@@ -1,7 +1,13 @@
 const inquirer = require('inquirer');
 
+let index = 0;
+/* eslint-disable */
 const ask = async config => {
+  if (!config.name) {
+    config.name = `inquirer_${index++}`;
+  }
   const answer = await inquirer.prompt(config);
+
   const result = answer[config.name];
   if (typeof config.dataTransform === 'function') {
     return config.dataTransform(result);
